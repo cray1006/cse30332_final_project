5ad731abca42436e406322af2321d05658c87df3
Christopher William Ray <cray@student00.cse.nd.edu>
2015-05-06 00:45:53 -0400

    added attack classes

--- creature.py
+++ creature.py
@@ -8,7 +8,7 @@ import sys, pygame, math, time, os
 from pygame.locals import *
 
 class fireball(pygame.sprite.Sprite):
-	def __init__(self, player, gs = None):
+	def __init__(self, player, x, y, gs = None):
 		self.gs = gs
 		self.image = pygame.image.load('fireball.png')
 
@@ -16,13 +16,128 @@ class fireball(pygame.sprite.Sprite):
 			self.image = pygame.transform.flip(self.image, 1, 0)
 
 		self.rect = self.image.get_rect()
+		self.rex.x = x
+		self.rect.y = y
 		color = self.image.get_at((0,0))
 		self.image.set_colorkey(color)
+		self.player = player
+
+	def move(self):
+		if(self.player == 0):
+			self.rect.x += 100
+		else:
+			self.rect.x -= 100
+
 
 class pyro(pygame.sprite.Sprite):
+	def __init__(self, player, x, y, gs = None):
+		self.gs = gs
+		self.image = pygame.image.load('pyroblast.png')
+
+		if(player != 0):
+			self.image = pygame.transform.flip(self.image, 1, 0)
+
+		self.rect = self.image.get_rect()
+		self.rex.x = x
+		self.rect.y = y
+		color = self.image.get_at((0,0))
+		self.image.set_colorkey(color)
+		self.player = player
+
+	def move(self):
+		if(self.player == 0):
+			self.rect.x += 100
+		else:
+			self.rect.x -= 100
+
+class ice(pygame.sprite.Sprite):
+	def __init__(self, player, x, y, gs = None):
+		self.gs = gs
+		self.image = pygame.image.load('ice_lance.png')
+
+		if(player != 0):
+			self.image = pygame.transform.flip(self.image, 1, 0)
+
+		self.rect = self.image.get_rect()
+		self.rex.x = x
+		self.rect.y = y
+		color = self.image.get_at((0,0))
+		self.image.set_colorkey(color)
+		self.player = player
+
+	def move(self):
+		if(self.player == 0):
+			self.rect.x += 100
+		else:
+			self.rect.x -= 100
+
+
+class freeze(pygame.sprite.Sprite):
+	def __init__(self, player, x, y, gs = None):
+		self.gs = gs
+		self.image = pygame.image.load('freeze.png')
+
+		if(player != 0):
+			self.image = pygame.transform.flip(self.image, 1, 0)
+
+		self.rect = self.image.get_rect()
+		self.rex.x = x
+		self.rect.y = y
+		color = self.image.get_at((0,0))
+		self.image.set_colorkey(color)
+		self.player = player
+
+	def move(self):
+		if(self.player == 0):
+			self.rect.x += 100
+		else:
+			self.rect.x -= 100
+
+
+class vine(pygame.sprite.Sprite):
+	def __init__(self, player, x, y, gs = None):
+		self.gs = gs
+		self.image = pygame.image.load('vine_whip.png')
+
+		if(player != 0):
+			self.image = pygame.transform.flip(self.image, 1, 0)
+
+		self.rect = self.image.get_rect()
+		self.rex.x = x
+		self.rect.y = y
+		color = self.image.get_at((0,0))
+		self.image.set_colorkey(color)
+		self.player = player
+
+	def move(self):
+		if(self.player == 0):
+			self.rect.x += 100
+		else:
+			self.rect.x -= 100
+
+
+class giga(pygame.sprite.Sprite):
+	def __init__(self, player, x, y, gs = None):
+		self.gs = gs
+		self.image = pygame.image.load('giga_drain.png')
+
+		if(player != 0):
+			self.image = pygame.transform.flip(self.image, 1, 0)
+
+		self.rect = self.image.get_rect()
+		self.rex.x = x
+		self.rect.y = y
+		color = self.image.get_at((0,0))
+		self.image.set_colorkey(color)
+		self.player = player
+
+	def move(self):
+		if(self.player == 0):
+			self.rect.x += 100
+		else:
+			self.rect.x -= 100
 	
 		
-
 class Water(pygame.sprite.Sprite):
 	def __init__(self, player, gs = None):
 		self.gs = gs
